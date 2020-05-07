e17f210377ca5d7554a69ba395ea583ce1642562
WBMeitei <60611084+WBMeitei@users.noreply.github.com>
2020-05-07 13:05:34 +0530

    Add files via upload

---
+++ WBMeitei.R
@@ -0,0 +1,11 @@
+library("rmarkdown", lib.loc="~/R/win-library/3.6")
+rmarkdown::render_site()
+
+#<h2><span style="color: blue;font-size: 23px"><strong>ꯋꯥꯍꯦꯡꯕꯝ ꯕꯤꯒ꯭ꯌꯥꯅꯟꯗꯥ ꯃꯧꯇꯩ</strong></span></h2>      (Font Size - 17)
+#<h3><span style="color: blue; font-family: 'Noto Sans Meetei Mayek', sans-serif; src = @import url(//fonts.googleapis.com/earlyaccess/notosansmeeteimayek.css);"><strong>ꯋꯥꯍꯦꯡꯕꯝ ꯕꯤꯒ꯭ꯌꯥꯅꯟꯗꯥ ꯃꯧꯇꯩ</strong></span></h3>
+
+#<span style="color: black;">For detailed information, please go through my CV ([Cick Here](files\CURRICULAM VITAE_wbm.pdf)).</span>
+
+#<img src="images\o1n.jpg" style="width:auto; border:0px solid; margin-right:0px" align="left">
+
+#https://www.smashingmagazine.com/2012/05/building-real-time-commenting-system/
